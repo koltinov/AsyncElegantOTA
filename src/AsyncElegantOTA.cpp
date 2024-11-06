e006901ccaee8af1fe291e3a01f2b89ed34fcd74
koltinov <97818885+koltinov@users.noreply.github.com>
2024-11-06 20:47:47 +0500

    Update AsyncElegantOTA.cpp
    id

--- src/AsyncElegantOTA.cpp
+++ src/AsyncElegantOTA.cpp
@@ -122,8 +122,7 @@ String AsyncElegantOtaClass::getID(){
     #if defined(ESP8266)
         id = String(ESP.getChipId());
     #elif defined(ESP32)
-        id = String((uint32_t)ESP.getEfuseMac(), HEX);
+        id = String(WiFi.getHostname());
     #endif
-    id.toUpperCase();
     return id;
 }
